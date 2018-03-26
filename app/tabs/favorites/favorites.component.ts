ab9aefb148b04a17fa4abaa62d4ccea19516bd32
Derek Hannah <drkhannah1@gmail.com>
2018-03-26 09:07:20 -0400

    Initial commit

---
+++ app/tabs/favorites/favorites.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "Favorites",
+    moduleId: module.id,
+    templateUrl: "./favorites.component.html"
+})
+export class FavoritesComponent implements OnInit {
+    constructor() {
+        /* ***********************************************************
+        * Use the constructor to inject services.
+        *************************************************************/
+    }
+
+    ngOnInit(): void {
+        /* ***********************************************************
+        * Use the "ngOnInit" handler to initialize data for the view.
+        *************************************************************/
+    }
+}
---
+++ app/tabs/tabs.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "nativescript-angular/common";
+
+import { BrowseComponent } from "./browse/browse.component";
+import { FavoritesComponent } from "./favorites/favorites.component";
+import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from "./search/search.component";
+import { TabsRoutingModule } from "./tabs-routing.module";
+import { TabsComponent } from "./tabs.component";
+
+@NgModule({
+    imports: [
+        NativeScriptCommonModule,
+        TabsRoutingModule
+    ],
+    declarations: [
+        TabsComponent,
+        HomeComponent,
+        BrowseComponent,
+        FavoritesComponent,
+        SearchComponent
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
+})
+export class TabsModule { }
